913538d9a4161529725d8223423f1de6a4dd5393
Cole Leisen <coleleisen@gmail.com>
2020-01-22 20:22:14 -0500

    standings sort

--- src/StandingsTable.js
+++ src/StandingsTable.js
@@ -2,8 +2,27 @@ import React, { Component } from "react";
 
 class StandingsTable extends Component {
 
+    constructor(props){
+        super(props)
+        this.state={
+            selected : -1,
+            teams : [],
+            og : []
+        }
+    }
+    componentDidMount(){
+        let oger = JSON.parse(JSON.stringify(this.props.teams));
+        this.setState({teams : this.props.teams, og : oger});
+    }
     sortTable = (index)=>{
-        console.log("bounjour " + index)
+        let table = this.state.teams;
+        if(index==1000){
+            table = JSON.parse(JSON.stringify(this.state.og));
+        }
+        else{
+            table.sort((a,b) =>  b.catAvg[index].number  - a.catAvg[index].number );
+        }
+        this.setState({teams : table, selected : index})
     }
   render() {
     return (
@@ -11,27 +30,29 @@ class StandingsTable extends Component {
     <table class="table">
     <thead class="thead-dark">
     <tr>
-        <th scope="col" onClick={()=> this.sortTable(1000)} > TEAM </th>
+        <th scope="col" key={1000} style={{textAlign : "center"}} data-column={1000} onClick={()=> this.sortTable(1000)} ><p>TEAM <i class="glyphicon glyphicon-sort"></i></p></th>
          
         {this.props.leagueAverage.map((cat, i)=>{
-          return(   
-          <th key={i} scope="col" onClick={()=> this.sortTable(i)}>{cat.name}</th>
-          )})}
+            let e;
+            {this.state.selected!==i ? e=<th key={i} scope="col" style={{textAlign : "center"}} data-column={i} onClick={()=> this.sortTable(i)}>{cat.name} <i class="glyphicon glyphicon-sort"></i></th> :  
+            e = <th key={i} scope="col" style={{textAlign : "center", backgroundColor: "red"}} data-column={i} onClick={()=> this.sortTable(i)}>{cat.name} <i class="glyphicon glyphicon-sort"></i></th> }
+
+          return e; 
+          })}
     </tr>
   </thead>
   <tbody>
       
-      {this.props.teams.map((team, i)=>{
+      {this.state.teams.map((team, i)=>{
           return(
-            <tr>
-              <td key={i} scope="row">{team.teamName.substr(1,team.teamName.length)}</td>
+            <tr key={i}>
+              <td key={10+i} data-column={1000} scope="row"><p>{team.teamName.substr(2,team.teamName.length)}</p></td>
                 
               {team.catAvg.map((cat, index)=>{
-                  return(
-                      
-                  <td key={index} scope="col" >{cat.number}</td>
-                  
-                  )
+                  let e;
+                  {this.state.selected!==index ? e= <td key={i*100+index} data-column={index} scope="col" ><p>{cat.number}</p></td> :  
+                    e= <td key={i*100+index} data-column={index} style={{backgroundColor : "red"}}scope="col" ><p>{cat.number}</p></td>}
+                return e; 
               })}          
               
               </tr>
